10a89975389cf084245ba39e8456bf7345edc4a7
Andreas Bresser <self@andreasbresser.de>
2024-01-26 23:27:15 +0100

    create round island shape

--- Scripts/LevelCreation/LevelCreation.gd
+++ Scripts/LevelCreation/LevelCreation.gd
@@ -2,10 +2,10 @@ extends Node3D
 
 var tile_scenes = []
 
-func hex_to_pos(row, col, hexagon_size):
-	var vertical_spacing = hexagon_size
-	var horizontal_spacing = 1.12 * hexagon_size
-	var horizontal_offset = 0.5 * hexagon_size if row % 2 != 0 else 0 
+func hex_to_pos(row, col):
+	var vertical_spacing = 1.65
+	var horizontal_spacing = 2.0
+	var horizontal_offset = 0.5 * vertical_spacing if row % 2 != 0 else 0 
 
 	var x = col * horizontal_spacing + horizontal_offset
 	var y = row * vertical_spacing
@@ -15,13 +15,27 @@ func hex_to_pos(row, col, hexagon_size):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	tile_scenes.append(preload("res://Scenes/tiles/grassed_tile.tscn"))
+	tile_scenes.append(preload("res://Scenes/tiles/grassed_tile_max.tscn"))
+	tile_scenes.append(preload("res://Scenes/tiles/grassed_tile_mid.tscn"))
+	tile_scenes.append(preload("res://Scenes/tiles/grassed_tile_min.tscn"))
+	tile_scenes.append(preload("res://Scenes/tiles/sand_tile.tscn"))
 	
-	for x in range(100):
-		for y in range(100):
+	for x in range(-100, 100):
+		for y in range(-100, 100):
+			var pos = hex_to_pos(x, y)
+			var dist = sqrt(pos[0]**2 + pos[1]**2)
 			var tile = tile_scenes[0]
+			if dist > 30:
+				continue
+			elif dist > 28:
+				tile = tile_scenes[3]
+			elif dist > 25:
+				tile = tile_scenes[2]
+			elif dist > 20:
+				tile = tile_scenes[1]
+
+			
 			var instance = tile.instantiate()
-			var pos = hex_to_pos(x, y, 1.8)
 			instance.position = Vector3(pos[0], 0, pos[1])
 			add_child(instance)
 			
