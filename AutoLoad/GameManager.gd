a7f98d42ef3aafbc16d5bc7b747d03e5f085a765
Jonte Boronowsky <jonte1@uni-bremen.de>
2024-01-27 12:30:10 +0100

    add messages on game mode start

--- AutoLoad/GameManager.gd
+++ AutoLoad/GameManager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 signal points_updated(player_id: int, points: int)
+signal game_mode_updated(game_mode: GameMode)
 
 var players: Dictionary = {}
 var points_per_player: Dictionary = {}
