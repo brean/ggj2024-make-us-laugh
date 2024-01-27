72cbad9ebc3360078251a29585815a44c193482e
SoldatenEnte <75547159+SoldatenEnte@users.noreply.github.com>
2024-01-27 15:22:01 +0100

    Point Select

---
+++ Scenes/UI/start_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+var max_points = 10
+
+func _on_play_pressed():
+	if(Input.get_connected_joypads().size() > 1):
+		get_tree().change_scene_to_file("res://battle_scene.tscn")
+
+
+func _on_button_pressed():
+	if(max_points < 50):
+		max_points += 10
+	else:
+		max_points = 10
+	get_node("MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/Button").text = str(max_points)
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
