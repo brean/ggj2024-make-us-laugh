884387ed7c44842528b1a78c8112300ec8591b66
Jonte Boronowsky <jonte1@uni-bremen.de>
2024-01-27 14:51:05 +0100

    add last one standing mode

--- Player/player.gd
+++ Player/player.gd
@@ -10,6 +10,7 @@ const OriginalAcc := 14.5
 const OriginalJumpImpulse := 250.0
 const RotationSpeed := 0.2
 const IdleThreshold := 0.01
+const OriginalKnockbackModifier := 1
 
 enum PlayerStates {IDLE, MOVE, JUMP, FALL, ATTACK, DUMMY}
 
@@ -19,6 +20,7 @@ enum PlayerStates {IDLE, MOVE, JUMP, FALL, ATTACK, DUMMY}
 @export var acceleration := self.OriginalAcc
 @export var jump_impulse := self.OriginalJumpImpulse
 @export var current_state : PlayerStates = self.PlayerStates.IDLE
+@export var knockback_mod : float = 1
 
 var direction := Vector2.ZERO
 var old_velocity_xz := Vector2.ZERO
@@ -200,6 +202,7 @@ func fall_state():
 	if self.ground_cast.is_colliding():
 		self.change_state(PlayerStates.MOVE)
 
-func reset_speed_mods():
+func reset_modifiers():
 	max_speed = OriginalSpeed
 	acceleration = OriginalAcc
+	knockback_mod = OriginalKnockbackModifier
