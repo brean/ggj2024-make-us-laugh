3d7a92d0baaa750a95ae01f986f61416c1847b68
Andreas Bresser <self@andreasbresser.de>
2024-01-27 10:44:37 +0100

    update tiles

--- Scripts/Level/LevelManager.gd
+++ Scripts/Level/LevelManager.gd
@@ -1,8 +1,11 @@
 extends Node3D
 
 var tile_scenes = []
+var tiles = []
 
 func hex_to_pos(row, col):
+	# calculate x and y position in meter based on position in the grid
+	var size = 2.31 / 2.0  # dimensions measured in Blender
 	var vertical_spacing = 1.65
 	var horizontal_spacing = 2.0
 	var horizontal_offset = 0.5 * vertical_spacing if row % 2 != 0 else 0 
@@ -13,34 +16,46 @@ func hex_to_pos(row, col):
 	return [x, y]
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func tile_creation():
 	tile_scenes.append(preload("res://Scenes/tiles/grassed_tile_max.tscn"))
 	tile_scenes.append(preload("res://Scenes/tiles/grassed_tile_mid.tscn"))
 	tile_scenes.append(preload("res://Scenes/tiles/grassed_tile_min.tscn"))
 	tile_scenes.append(preload("res://Scenes/tiles/sand_tile.tscn"))
 	
-	for x in range(-100, 100):
-		for y in range(-100, 100):
+	var MAX_RADIUS = 18
+	for x in range(-MAX_RADIUS, MAX_RADIUS):
+		for y in range(-MAX_RADIUS, MAX_RADIUS):
 			var pos = hex_to_pos(x, y)
 			var dist = sqrt(pos[0]**2 + pos[1]**2)
 			var tile = tile_scenes[0]
-			if dist > 30:
+			if dist > MAX_RADIUS:
 				continue
-			elif dist > 28:
+			elif dist > MAX_RADIUS-2:
 				tile = tile_scenes[3]
-			elif dist > 25:
+			elif dist > MAX_RADIUS-4:
 				tile = tile_scenes[2]
-			elif dist > 20:
+			elif dist > MAX_RADIUS-7:
 				tile = tile_scenes[1]
 
 			
 			var instance = tile.instantiate()
 			instance.position = Vector3(pos[0], 0, pos[1])
 			add_child(instance)
+			tiles.append(instance)
+
+
+func reset_tiles():
+	for tile in tiles:
+		var position = tile.position
+		# position.y = randi() % 3
+		position.y = 0
+		tile.position = position
+
+
+func _ready():
+	tile_creation()
+	reset_tiles()
 			
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
