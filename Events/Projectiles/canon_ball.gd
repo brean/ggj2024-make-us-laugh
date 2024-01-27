1c2919a217abd312043dfb1c8b0218fdc22cc87c
Andreas Bresser <self@andreasbresser.de>
2024-01-27 23:14:23 +0100

    fix cannon ball

--- Events/Projectiles/canon_ball.gd
+++ Events/Projectiles/canon_ball.gd
@@ -1,21 +1,27 @@
 extends RigidBody3D
 
-var elements = [
+@onready var elements = [
 	[$tomato, $CollisionSphere],
 	[$burger, $CollisionSphere],
 	[$fridge, $CollisionBox],
 ]
 
-func disable_all():
-	for elem in elements:
-		elem[0].disabled = true
-		elem[1].disabled = true
-
 func random_element():
-	disable_all()
-	var this_elem = elements[randi() % elements.size()]
-	this_elem[0].disabled = false;
-	this_elem[1].disabled = false;
+	var desired = randi() % elements.size()
+	var removed_elems = []
+	for i in range(elements.size()):
+		if i == desired:
+			continue
+		var this_elem = elements[i]
+		for elem in elements[i]:
+			if elem in removed_elems:
+				continue
+			remove_child(elem)
+			removed_elems.append(elem)
+
 
 func _on_timer_timeout():
 	self.queue_free()
+
+func _ready():
+	random_element()
