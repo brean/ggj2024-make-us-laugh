7e091a65c28f72696527a441a2d1d8c0cf067ece
Tom Freudenberg <tom.freudenberg98@gmail.com>
2024-01-28 15:18:40 +0100

    Fix gamemode timer

--- GameModeController/GameModeController.gd
+++ GameModeController/GameModeController.gd
@@ -3,14 +3,17 @@ extends Node3D
 @export var delay_between_modes: float = 1
 @export var game_modes: Array[PackedScene] # array from which game modes are selected
 
+@onready var delay_timer = $DelayTimer
+
 func _ready():
 	on_game_mode_finished()
 
 func on_game_mode_finished():
 	GameManager.game_mode_exited.emit()
-	
-	await get_tree().create_timer(delay_between_modes).timeout
-	
+	self.delay_timer.start(self.delay_between_modes)
+
+
+func _on_delay_timer_timeout():
 	var game_mode_scene = game_modes.pick_random() # choose game mode at random
 	var game_mode: GameMode = game_mode_scene.instantiate()
 	game_mode.finished.connect(on_game_mode_finished)
