6f69fb7bb931e718d40dd844ca209f3554855210
Andreas Bresser <self@andreasbresser.de>
2024-01-28 23:12:48 +0100

    keyboard and old joypad controls

--- Scenes/UI/start_menu.gd
+++ Scenes/UI/start_menu.gd
@@ -18,87 +18,8 @@ func _ready():
 	$Mage.set_visible(false)
 	$Knight.set_visible(false)
 	$Barbarian.set_visible(false)
-	
-func _process(delta):
-	if MultiplayerInput.is_action_just_pressed(0, "ui_left"):
-		if(focused_button == point_button):
-			focused_button = play_button
-			focused_button.grab_focus()
-	if MultiplayerInput.is_action_just_pressed(0, "ui_right"):
-		if(focused_button == play_button):
-			focused_button = point_button
-			focused_button.grab_focus()
-	if MultiplayerInput.is_action_just_pressed(0, "ui_up"):
-		if(focused_button == quit_button):
-			focused_button = play_button
-			focused_button.grab_focus()
-	if MultiplayerInput.is_action_just_pressed(0, "ui_down"):
-		if(focused_button == play_button || focused_button == point_button):
-			focused_button = quit_button
-			focused_button.grab_focus()
-	if MultiplayerInput.is_action_just_pressed(0, "Jump"):
-		if(focused_button == play_button):_on_play_pressed()
-		if(focused_button == point_button):_on_button_pressed()
-		if(focused_button == quit_button):_on_quit_pressed()
-	
-	if(MultiplayerInput.handled_devices[0] && !joined[0]):
-		joined[0] = true;
-		$Rogue_Hooded.set_visible(true)
-		$Rogue_Hooded.animation_player.play("Cheer")
-		$MarginContainer/HBoxContainer/MarginContainer/connect_status.text = "Connected"
-	elif(!MultiplayerInput.handled_devices[0]):
-		$MarginContainer/HBoxContainer/MarginContainer/connect_status.text = "Pending..."
-		
-	if(MultiplayerInput.handled_devices[1] && !joined[1]):
-		joined[1] = true;
-		$Mage.set_visible(true)
-		$Mage.animation_player.play("Cheer")
-		$MarginContainer/HBoxContainer/MarginContainer2/connect_status.text = "Connected"
-	elif(!MultiplayerInput.handled_devices[1]):
-		$MarginContainer/HBoxContainer/MarginContainer2/connect_status.text = "Pending..."
-		
-	if(MultiplayerInput.handled_devices[2] && !joined[2]):
-		joined[2] = true;
-		$Knight.set_visible(true)
-		$Knight.animation_player.play("Cheer")
-		$MarginContainer/HBoxContainer/MarginContainer3/connect_status.text = "Connected"
-	elif(!MultiplayerInput.handled_devices[2]):
-		$MarginContainer/HBoxContainer/MarginContainer3/connect_status.text = "Pending..."
-		
-	if(MultiplayerInput.handled_devices[3] && !joined[3]):
-		joined[3] = true;
-		$Barbarian.set_visible(true)
-		$Barbarian.animation_player.play("Cheer")
-		$MarginContainer/HBoxContainer/MarginContainer4/connect_status.text = "Connected"
-	elif(!MultiplayerInput.handled_devices[3]):
-		$MarginContainer/HBoxContainer/MarginContainer4/connect_status.text = "Pending..."
-	
-	if(MultiplayerInput.handled_devices[0] && MultiplayerInput.is_action_pressed(0, "Punch")):
-		$Rogue_Hooded.animation_player.play("Cheer")
-	elif($Rogue_Hooded.animation_player.get_current_animation() != "Cheer"):
-		$Rogue_Hooded.animation_player.play("Idle")	
-		
-	if(MultiplayerInput.handled_devices[1] && MultiplayerInput.is_action_pressed(1, "Punch")):
-		$Mage.animation_player.play("Cheer")
-	elif($Mage.animation_player.get_current_animation() != "Cheer"):
-		$Mage.animation_player.play("Idle")	
-		
-	if(MultiplayerInput.handled_devices[2] && MultiplayerInput.is_action_pressed(2, "Punch")):
-		$Knight.animation_player.play("Cheer")
-	elif($Knight.animation_player.get_current_animation() != "Cheer"):
-		$Knight.animation_player.play("Idle")	
-		
-	if(MultiplayerInput.handled_devices[3] && MultiplayerInput.is_action_pressed(3, "Punch")):
-		$Barbarian.animation_player.play("Cheer")
-	elif($Barbarian.animation_player.get_current_animation() != "Cheer"):
-		$Barbarian.animation_player.play("Idle")	
 
-	if(MultiplayerInput.handled_devices[3] && !MultiplayerInput.handled_devices[4]):
-		play_button.disabled = false
-	else:
-		play_button.disabled = true
-	
-	
+
 func _on_play_pressed():
 	if(Input.get_connected_joypads().size() == 4):
 		GameManager.max_points = max_points
--- Scenes/UI/title_screen.gd
+++ Scenes/UI/title_screen.gd
@@ -11,19 +11,20 @@ func to_idle():
 	self.canvas_layer.visible = true
 
 
+func check_input(dev_idx):
+	for key in ["Left", "Right", "Forward", "Backward", "Jump", "Punch"]:
+		if MultiplayerInput.is_action_just_pressed(dev_idx, key):
+			self.to_start_menu(dev_idx)
+
 func _process(_delta):
-	if MultiplayerInput.handled_devices[0]:
-		if MultiplayerInput.is_action_just_pressed(0, "ui_left"):
-			self.to_start_menu()
-		if MultiplayerInput.is_action_just_pressed(0, "ui_right"):
-			self.to_start_menu()
-		if MultiplayerInput.is_action_just_pressed(0, "ui_up"):
-			self.to_start_menu()
-		if MultiplayerInput.is_action_just_pressed(0, "ui_down"):
-			self.to_start_menu()
-		if MultiplayerInput.is_action_just_pressed(0, "Jump"):
-			self.to_start_menu()
+	# check joypads
+	for dev_idx in MultiplayerInput.handled_devices.size():
+		if not MultiplayerInput.handled_devices[dev_idx]:
+			continue
+		check_input(dev_idx)
+	# check keyboard
+	check_input(-1)
 
 
-func to_start_menu():
+func to_start_menu(main_dev_idx):
 	get_tree().change_scene_to_file("res://Scenes/UI/start_menu.tscn")
