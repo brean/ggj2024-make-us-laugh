074c786fa9958a58e2d759d201d2221aaf87823f
Tom Freudenberg <tom.freudenberg98@gmail.com>
2024-01-28 14:41:46 +0100

    Add title screen

--- GameMode/coin_mode.gd
+++ GameMode/coin_mode.gd
@@ -9,7 +9,7 @@ func start():
 	var spawn_n = randi_range(-3, 3) + self.number_of_coins
 	for i in range(spawn_n):
 		var new_coin = Coin.instantiate()
-		self.add_child(new_coin)
+		self.get_parent().add_child(new_coin)
 		new_coin.global_position = Vector3(
 			randf_range(-self.spawn_range, self.spawn_range), 
 			randf_range(8.0, 12.0), 
